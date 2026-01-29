102246fbd4811d8a68d22bc810c304b8e9d1c859
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-01-29 18:48:21 +0600

    convert string to array

---
+++ split.js
@@ -0,0 +1,13 @@
+/// split method very for reverse a string
+let str1='We Love Programing';
+let strArray= str1.split(' L');
+// console.log(strArray);
+let reverseStrArray=strArray.reverse();
+// console.log(reverseStrArray);
+let str3 ='I love my Mother';
+let str3Array= str3.split(" ");
+console.log(str3Array);
+let reverseStr= str3Array.reverse();
+// console.log(reverseStr);
+let joinStr= reverseStr.join(" ");
+console.log(joinStr);
