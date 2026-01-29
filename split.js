66a15fa8b61120518053cfb08367929a635714bc
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-01-29 19:03:06 +0600

    some thing updated

--- split.js
+++ split.js
@@ -5,9 +5,9 @@ let strArray= str1.split(' L');
 let reverseStrArray=strArray.reverse();
 // console.log(reverseStrArray);
 let str3 ='I love my Mother';
-let str3Array= str3.split(" ");
+let str3Array= str3.split(" ");// split work for string to array convert
 console.log(str3Array);
 let reverseStr= str3Array.reverse();
 // console.log(reverseStr);
-let joinStr= reverseStr.join(" ");
+let joinStr= reverseStr.join(" "); //join work for array to srting convert
 console.log(joinStr);
