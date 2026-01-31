8c40574999c2cf3b65e487fb54c7a2135c075d22
abdulkarim70 <mmd.abdulkarim@gmail.com>
2026-01-31 16:43:36 +0600

    practise

---
+++ newob.js
@@ -0,0 +1,119 @@
+// let computer={
+//     brand:'Dell',
+//     price:25000,
+//     Motherboard: 'Intel',
+//     isUpdatedModel:true
+// }
+// let keys = Object.keys(computer)
+// let values =Object.values(computer)
+// for(let i=0; i<keys.length;i++){
+//     let key =keys[i]
+//     let value= computer[key]
+//     console.log(key, ": ", value)
+// 
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+// let revColor =[];
+// for(let i=0;i<colors.length;i++){
+//     let color =colors[i];
+//    revColor.unshift(color)
+// }
+//  console.log(revColor)
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+// for (let i= 0; i<numbers.length;i++){
+//     let num=numbers[i]
+//     if(num%2==0){
+//         console.log(num)
+//     }
+// }
+// var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+// // let result ='';
+// for(let n of numbers){
+//     result+=n;
+// }
+// console.log(result)
+// let result = numbers.join('')
+// console.log(result);
+// console.log(numbers)
+
+// const statement = 'I am a hard working person';
+// let stringToArray= statement.split(' '); // string to array conversation
+// let ReverseWord=stringToArray.reverse();// রিভার্স দিয়ে ওয়ার্ডগুলো উল্টালাম
+// let joinWord =ReverseWord.join(' ');
+// console.log(joinWord)
+
+// const statement = 'I am a hard working person';
+
+// let stringToArray= statement.split(' '); // string to array convert
+// let revWord= stringToArray.reverse(); //reverse
+// let joinStr= revWord.join(' '); // join korlam
+// console.log(joinStr);
+
+// let original =[1, 2, 3];
+// let copy = original.slice();
+// copy[0]=99;
+// console.log("original :", original)
+// console.log("Copy;" , copy)
+
+// const students=[{ name: "John", marks: 85 },
+//   { name: "Alice", marks: 90 }];
+//  for(let student of students){
+//     console.log(student.name+ " ","Scored"," ",+ student.marks )
+    
+//  }
+// const colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520'
+// };
+// console.log(colors["golden rod"])
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+
+// };
+// car.passengerCapacity=5,
+// console.log(car)
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+// console.log(student.physics.marks)
+// let student = {
+//     name: 'Ariana Grande',
+//     age: 21,
+//     city: 'Gaibandha',
+//     isStudent: true
+// };
+// let count =Object.keys(student); //age array te rupantor korlam 
+// console.log(count.length)
+// let myObject = {
+
+// name: 'John Doe',
+// age: 25,
+// city: 'Example City',
+// isStudent: true
+// };
+// for (let key in myObject){
+//     let value=myObject[key]
+//     console.log()
+// }
+// let num =0;
+// for(let i=0 ; i<200; i++){
+//     num=[i]
+//     if(num>=100){
+//         break
+//     }
+//     console.log(num)
+// }
+let sum=0;
+while(sum<1){
+    sum++
+}
